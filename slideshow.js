d6a0641d8c4231eac793909fdacdc16e3e666a76
EriLan4 <112878245+EriLan4@users.noreply.github.com>
2023-02-14 10:33:28 +0100

    added .js files, added picture files
    moved javascript to .js files, changed all pictures to new ones, added picture slideshow in index.html

---
+++ slideshow.js
@@ -0,0 +1,22 @@
+  // Script from https://www.w3schools.com/howto/howto_js_slideshow.asp
+let slideIndex = 1
+showSlides(slideIndex);
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    if (n > slides.length) {slideIndex = 1}    
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slides[slideIndex-1].style.display = "block"
+}
